50535731775391e12035c7c1f6ec92a2d3e42215
LeeJaeEun0 <wodmschlrh12@naver.com>
2023-08-12 23:31:02 +0900

    PostReposity에서 deleteAt의 값이 null인 경우만 출력하도록 설정

--- src/main/java/com/agile/demo/persistence/PostReposity.java
+++ src/main/java/com/agile/demo/persistence/PostReposity.java
@@ -3,5 +3,8 @@ package com.agile.demo.persistence;
 import com.agile.demo.model.PostEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostReposity extends JpaRepository<PostEntity, Long> {
+    List<PostEntity> findByDeleteAtIsNull();
 }
--- src/main/java/com/agile/demo/service/PostService.java
+++ src/main/java/com/agile/demo/service/PostService.java
@@ -6,6 +6,7 @@ import com.agile.demo.persistence.PostReposity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -27,7 +28,30 @@ public class PostService {
     }
 
     public List<PostEntity> getAllPost() {
-        List<PostEntity> postEntities = postReposity.findAll();
+        List<PostEntity> postEntities = postReposity.findByDeleteAtIsNull(); // delete가 null인 경우만 출력하기
+
         return postEntities;
+    }
+
+    public PostEntity updatePost(PostDto postDto, Long number){
+        //1. 비밀번호가 일치하는지 확인
+        
+        //2. 내용을 수정함
+        PostEntity postEntity = postReposity.getById(number);
+        postEntity.setTitle(postDto.getTitle());
+        postEntity.setContext(postDto.getContext());
+        postEntity.setWriter(postDto.getWriter());
+        postEntity.setPassword(postDto.getPassword());
+
+        return postReposity.save(postEntity);
+    }
+
+    public PostEntity deletePost(Long number){
+        //1. 비밀번호가 일치하는지 확인 
+        
+        //2. deleteAt에 삭제 날짜 넣기
+        PostEntity postEntity = postReposity.getById(number);
+        postEntity.setDeleteAt(LocalDateTime.now());
+        return postReposity.save(postEntity);
     }
 }
