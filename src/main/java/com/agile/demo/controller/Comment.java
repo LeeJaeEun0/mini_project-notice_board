d0e1b4e796ca0d6a86ff383529a292f91845ab43
LeeJaeEun0 <wodmschlrh12@naver.com>
2023-08-21 17:15:49 +0900

    주석으로 각 메서드 기능 설명

--- src/main/java/com/agile/demo/controller/Comment.java
+++ src/main/java/com/agile/demo/controller/Comment.java
@@ -15,24 +15,28 @@ public class Comment {
     @Autowired
     CommentService commentService;
 
+    //Post - 댓글 작성하기
     @PostMapping
     public ResponseEntity<?> createComment(@RequestBody CommentDto commentDto){
         CommentEntity commentEntity = commentService.createComment(commentDto);
         return ResponseEntity.accepted().build();
     }
 
+    // Post - 대댓글 작성하기
     @PostMapping("/{number}")
     public ResponseEntity<?> createSubComment(@PathVariable Long number, @RequestBody CommentDto commentDto){
         CommentEntity commentEntity = commentService.createSubComment(number,commentDto);
         return ResponseEntity.accepted().build();
     }
 
+    // Get - 모든 댓글 조회
     @GetMapping
     public List<?> getAllComment(){
         List<CommentEntity> comments = commentService.getAllComment();
         return comments;
     }
 
+    // Get - 선택된 게시글의 댓글 조회
     @GetMapping("/{number}")
     public List<?> getComment(@PathVariable Long number){
         List<CommentEntity> comments = commentService.getComment(number);
--- src/main/java/com/agile/demo/controller/PostController.java
+++ src/main/java/com/agile/demo/controller/PostController.java
@@ -17,6 +17,7 @@ public class PostController {
     @Autowired
     PostService postService;
 
+    // Post - 게시글 작성
     @PostMapping
     public ResponseEntity<?> createPost(@RequestBody PostDto postDto){
         PostEntity postEntity = postService.createPost(postDto);
@@ -24,24 +25,28 @@ public class PostController {
         return ResponseEntity.accepted().build();
     }
 
+    // Get - 전체 글 조회
     @GetMapping
     public List<?> getAllPost(){
         List<PostEntity> postEntitys = postService.getAllPost();
         return postEntitys;
     }
 
+    // Get - 한 개의 글 조회
     @GetMapping("/view/{number}")
     public Optional<PostEntity> getOnePost(@PathVariable Long number){
         Optional<PostEntity> postEntitys = postService.getOnePost(number);
         return postEntitys;
     }
 
+    // Get - 검색하기
     @GetMapping("/{text}")
     public List<?> SearchPost(@PathVariable String text){
         List<PostEntity> postEntitys = postService.searchPost(text);
         return postEntitys;
     }
 
+    // Patch - 게시글 수정하기
     @PatchMapping("/{number}")
     public ResponseEntity<?> updatePost(@RequestBody PostDto postDto, @PathVariable Long number){
         PostEntity postEntity = postService.updatePost(postDto, number);
@@ -49,6 +54,7 @@ public class PostController {
 
     }
 
+    //Delete - 게시글 삭제하기
     @DeleteMapping("/{number}")
     public ResponseEntity<?> deletePost(@RequestBody PostDto postDto,@PathVariable Long number){
         PostEntity postEntity = postService.deletePost(postDto,number);
