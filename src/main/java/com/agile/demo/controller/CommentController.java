0ef8de87943b05bb1e245b451b8b0a537ccef3e2
LeeJaeEun0 <wodmschlrh12@naver.com>
2023-09-05 16:10:47 +0900

    파일명 수정

--- src/main/java/com/agile/demo/controller/CommentController.java
+++ src/main/java/com/agile/demo/controller/CommentController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/v1/comments")
-public class Comment {
+public class CommentController {
     @Autowired
     CommentService commentService;
 
