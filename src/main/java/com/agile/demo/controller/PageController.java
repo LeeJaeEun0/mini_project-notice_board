79a5ab223b5f8081435ea5b3eb7926137aad335c
LeeJaeEun0 <wodmschlrh12@naver.com>
2023-09-06 16:21:52 +0900

    input파일에 필요한 dto 미리 선언 하도록 수정 - Caused by: java.lang.IllegalStateException: Neither BindingResult nor plain target object for bean name 'postDto' available as request attribute 오류 해결

--- src/main/java/com/agile/demo/controller/PageController.java
+++ src/main/java/com/agile/demo/controller/PageController.java
@@ -1,12 +1,17 @@
 package com.agile.demo.controller;
 
+import com.agile.demo.dto.PostDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @Controller
 public class PageController {
 
     @GetMapping("/input")
-    public String showInputPage() {
+    public String showInputPage(Model model) {
+        model.addAttribute("postDto", new PostDto());
         return "input"; // Thymeleaf 템플릿 이름
     }
 
