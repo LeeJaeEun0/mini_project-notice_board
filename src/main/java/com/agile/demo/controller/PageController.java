4e6e9806c1de3320e5c517794b759247a4e3348c
LeeJaeEun0 <wodmschlrh12@naver.com>
2023-09-07 15:37:54 +0900

    password api 설계 - 수정전

--- src/main/java/com/agile/demo/controller/PageController.java
+++ src/main/java/com/agile/demo/controller/PageController.java
@@ -4,6 +4,7 @@ import com.agile.demo.dto.PostDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
@@ -15,4 +16,17 @@ public class PageController {
         return "input"; // Thymeleaf 템플릿 이름
     }
 
+    @GetMapping("/password/{number}")
+    public String showPasswordPage(Model model, @PathVariable Integer number) {
+        if (number == 1){ // 수정인 경우
+            model.addAttribute("number", 1);
+            model.addAttribute("postDto", new PostDto());
+            return "password";
+        } else if (number == 2) {// 삭제인 경우
+            model.addAttribute("number", 2);
+            model.addAttribute("postDto", new PostDto());
+            return "password";
+        }
+            return "board"; // 수정 필요
+    }
 }
