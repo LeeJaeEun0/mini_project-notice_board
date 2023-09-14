b5f5d5cc58e765ac4aea6e7b8b8420dc6d9794c2
LeeJaeEun0 <wodmschlrh12@naver.com>
2023-09-14 17:41:31 +0900

    검색하는 sql문 작성

--- src/main/java/com/agile/demo/persistence/PostReposity.java
+++ src/main/java/com/agile/demo/persistence/PostReposity.java
@@ -2,10 +2,15 @@ package com.agile.demo.persistence;
 
 import com.agile.demo.model.PostEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface PostReposity extends JpaRepository<PostEntity, Long> {
     List<PostEntity> findByDeleteAtIsNull();
     List<PostEntity> findByDeleteAtIsNullAndTitle(String text);
+
+    @Query("SELECT p FROM PostEntity p WHERE p.title like %:text% AND p.deleteAt is null")
+    List<PostEntity> findText(@Param("text") String text);
 }
